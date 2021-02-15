900adbd2f4ff6ea5df7562ca66a929dd7bf1845b
Ahmad Khattab <akhattab03@gmail.com>
2021-02-15 10:28:20 +0200

    adding home and trip layouts

---
+++ app/src/main/java/com/iti/gov/mashawery/AddTripActivity.java
@@ -0,0 +1,14 @@
+package com.iti.gov.mashawery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+}
--- app/src/main/java/com/iti/gov/mashawery/MainActivity.java
+++ app/src/main/java/com/iti/gov/mashawery/MainActivity.java
@@ -2,13 +2,20 @@ package com.iti.gov.mashawery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
