83e99584746da264fc6b8661d3734b61ef2daa5e
Doaa Gomaa <dgomaa49@gmail.com>
2021-02-15 10:39:39 +0200

    adding register and login and time picker

--- app/src/main/java/com/iti/gov/mashawery/RegisterActivity.java
+++ app/src/main/java/com/iti/gov/mashawery/RegisterActivity.java
@@ -3,20 +3,12 @@ package com.iti.gov.mashawery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.util.Calendar;
-
-public class MainActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
-
-
 }
