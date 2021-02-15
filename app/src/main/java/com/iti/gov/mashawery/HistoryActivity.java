d251c88d8b94b1a4c232ba0f2b9036ee99dcab22
Heba <hebahosain@gmail.com>
2021-02-15 11:11:02 +0200

    add notes and history layouts

--- app/src/main/java/com/iti/gov/mashawery/HistoryActivity.java
+++ app/src/main/java/com/iti/gov/mashawery/HistoryActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.insert_new_note);
+        setContentView(R.layout.activity_main);
     }
 }
